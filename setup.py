95b403799532beb9a2258a0eaf03fc7f1913f0e3
vijayshinde3004 <vijaymshinde96@gmail.com>
2023-06-11 16:10:57 +0530

    setup file code complited

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+requirement_file_name = "requirements.txt"
+REMOVE_PACKAGE = "-e ."
+
+def get_requirements() -> List[str]:
+    with open(requirement_file_name) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.strip() for requirement_name in requirement_list]
+
+    if REMOVE_PACKAGE in requirement_list:
+        requirement_list.remove(REMOVE_PACKAGE)
+    return requirement_list
+
+
+
+setup(name='Insurance',
+      version='0.0.1',
+      description='Insurance Premium Prediction',
+      author='vijay shinde',
+      author_email='vijaymshinde96@gmail.com',
+      packages=find_packages(),
+      install_requires=get_requirements()
+     )
